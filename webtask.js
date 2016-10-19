71ca7939f2a1949f3e499ac52b03e5ddbd4a5333
Adam Chin <chinjazz@gmail.com>
2016-10-19 10:24:45 -0400

    add slack token

--- webtask.js
+++ webtask.js
@@ -5,8 +5,8 @@ return function (context, callback) {
         console.log('Posting message to slack for repository ' 
             + context.webhook.repository.full_name);
         var url = 'https://slack.com/api/chat.postMessage'
-            + '?token=' + context.data.slack_token
-            + '&channel=' + context.data.slack_channel
+            + '?token=' + 'xoxp-15101058326-15101831218-93098380470-d30b4b9ecbd3a8d30de2f72cb73bccf3'
+            + '&channel=' + '#random'
             + '&user=' + (context.data.slack_user || 'WebTask')
             + '&text=' + encodeURIComponent('Changes in `' + context.webhook.repository.full_name + '`');
         request({ url: url, method: 'POST' }, function (error, res, body) {

62841eb0a5b08916525f1d24f47671b4ae43971e
Adam Chin <chinjazz@gmail.com>
2016-10-19 14:25:36 -0400

    just logging

--- webtask.js
+++ webtask.js
@@ -1,15 +1,10 @@
-var request = require('request');
-
 return function (context, callback) {
-
-    if (contxt.webhook){
-        request({ 
-        url:'https://hooks.slack.com/services/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', 
-        method: 'POST',
-        payload: {"channel": "#random", "text": "New Code has been committed to: " +context.webhook.repository.full_name }
-       
-        }, function (error, res, body) {
-            callback(error, body);
-        });
+    if (context.webhook) {
+        console.log('Repo: ' + context.webhook.repository.full_name);
+        console.log('Context data: ' + context.data);
+    }
+    else {
+        console.log('No webhook');
+        return callback();
     }
 }

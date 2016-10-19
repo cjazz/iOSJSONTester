c99f1a6b82881452cd149ad5fe5ac36c0b7aaddb
Adam Chin <chinjazz@gmail.com>
2016-10-19 14:29:43 -0400

    a

--- webtask.js
+++ webtask.js
@@ -1,7 +1,6 @@
 return function (context, callback) {
     if (context.webhook) {
         console.log('Repo: ' + context.webhook.repository.full_name);
-        console.log('Context data: ' + context.data);
     }
     else {
         console.log('No webhook');

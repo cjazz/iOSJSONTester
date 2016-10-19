818e10f60adf241923c052fa621cc41eda76a709
Adam Chin <chinjazz@gmail.com>
2016-10-19 13:42:51 -0400

    leverage nodemailer

--- webtask.js
+++ webtask.js
@@ -1,21 +1,30 @@
-// var request = require('request');
+var nodemailer = require('nodemailer');
 
 return function (context, callback) {
+    
     if (context.webhook) {
         console.log('Logging push for ' + context.webhook.repository.full_name);
-        console.log('Context data' + context.data);
+        console.log('context.webhook.info:  ' +context.webhook.info);
 
-        // context.webhook contains the webhook payload provided by GitHub
-        // context.data contains URL query and webtask token parameters
+        // create reusable transporter object using the default SMTP transport
+        var transporter = nodemailer.createTransport('smtps://user%40gmail.com:pass@smtp.gmail.com');
 
-        // var url = 'https://slack.com/api/chat.postMessage'
-        //     + '?token=' + context.data.slack_token
-        //     + '&channel=' + context.data.slack_channel
-        //     + '&user=' + (context.data.slack_user || 'WebTask')
-        //     + '&text=' + encodeURIComponent('Changes in `' + context.webhook.repository.full_name + '`');
-        // request({ url: url, method: 'POST' }, function (error, res, body) {
-        //     callback(error, body);
-        // });
+        // setup e-mail data with unicode symbols
+        var mailOptions = {
+            from: '"Adam Chin ?" <chinjazz@gmail.com>', // sender address
+            to: 'chinjazz@gmail.com, adam.chin@icloud.com', // list of receivers
+            subject: 'More Cowbell for ' + context.webhook.repository.full_name, // Subject line
+            text: 'More Cowbell  ?', // plaintext body
+            html: '<b>More Cowbell ?</b>' // html body
+        };
+
+        // send mail with defined transport object
+        transporter.sendMail(mailOptions, function(error, info){
+            if(error){
+                return console.log(error);
+            }
+            console.log('Message sent: ' + info.response);
+        });
     }
     else {
         console.log('No webhook');

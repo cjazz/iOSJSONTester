4f7f0efae8a6799b4cfd5fdbda60f8826da9e07f
Adam Chin <chinjazz@gmail.com>
2016-10-21 16:35:53 -0400

    yes

--- webtask.js
+++ webtask.js
@@ -1,27 +1,5 @@
 module.exports = function(context, callback) {
-    console.log('Repo ' +context.webhook.repository.full_name);
   // context.webhook contains the webhook payload provided by GitHub
   // context.data contains URL query and webtask token parameters
-  callback(null, { thing: 'results' });
-}        
-
-// var request = require('request');
-
-// return function (context, callback) {
-//     if (context.data.slack_token && context.data.slack_channel) {
-//         console.log('Posting message to slack for repository ' 
-//             + context.webhook.repository.full_name);
-//         var url = 'https://slack.com/api/chat.postMessage'
-//             + '?token=' + context.data.slack_token
-//             + '&channel=' + context.data.slack_channel
-//             + '&user=' + (context.data.slack_user || 'WebTask')
-//             + '&text=' + encodeURIComponent('Changes in `' + context.webhook.repository.full_name + '`');
-//         request({ url: url, method: 'POST' }, function (error, res, body) {
-//             callback(error, body);
-//         });
-//     }
-//     else {
-//         console.log('Repository ' + context.webhook.repository.full_name + ' changed but slack credentials not supplied.');
-//         return callback();
-//     }
-// }
+  callback(null, { some: 'result' });
+}   
